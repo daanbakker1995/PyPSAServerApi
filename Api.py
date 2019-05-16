94215b67267f33a84f209020434dc966540ba100
DaanBakker <daanbakker1995@users.noreply.github.com>
2019-05-16 12:50:40 +0200

    Update Api.py

--- Api.py
+++ Api.py
@@ -1,32 +1,9 @@
-from flask import Flask, jsonify, Response
-from flask_classful import FlaskView, route, request
-from flask_cors import CORS
-
-from PyPSAHandler import PyPSAHandler
-
+from flask import Flask
 app = Flask(__name__)
-CORS(app)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def hello_world():
+  return 'Hey its Python Flask application!'
 
-class Api(FlaskView):
-    route_base = '/api/'
-
-    @route('/calculation', methods=['POST'])
-    def calculate(self):
-        validationResult = PyPSAHandler.validate(request.get_json())
-        if(validationResult is not None):
-            return Response(validationResult, mimetype = 'text/plain', status = 406)
-
-        handler = PyPSAHandler(request.get_json())
-
-        calculationResult = handler.calculate()
-
-        if(calculationResult is None):
-            return Response("The submitted situation is not possible", mimetype='text/plain', status=400)
-        else:
-            return jsonify(calculationResult)
-
-Api.register(app)
+if __name__ == '__main__':
+  app.run()
