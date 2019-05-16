efed39dd34fbe9f4854c786d6488dbc8a41f45d1
DaanBakker <daanbakker1995@users.noreply.github.com>
2019-05-16 12:29:55 +0200

    Add files via upload

---
+++ Api.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, Response
+from flask_classful import FlaskView, route, request
+from flask_cors import CORS
+
+from PyPSAHandler import PyPSAHandler
+
+app = Flask(__name__)
+CORS(app)
+
+class Api(FlaskView):
+    route_base = '/api/'
+
+    @route('/calculation', methods=['POST'])
+    def calculate(self):
+        validationResult = PyPSAHandler.validate(request.get_json())
+        if(validationResult is not None):
+            return Response(validationResult, mimetype = 'text/plain', status = 406)
+
+        handler = PyPSAHandler(request.get_json())
+
+        calculationResult = handler.calculate()
+
+        if(calculationResult is None):
+            return Response("The submitted situation is not possible", mimetype='text/plain', status=400)
+        else:
+            return jsonify(calculationResult)
+
+Api.register(app)
